7fbd055b94355a4e66eeaae25932488a9754a3f5
phucbienvan <meotran0805@gmail.com>
2024-09-25 20:49:56 +0700

    create task

---
+++ app/Services/TaskService.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Services;
+
+use App\Models\Task;
+use Illuminate\Support\Facades\Log;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class TaskService {
+
+    protected $model;
+    public function __construct(Task $task)
+    {
+        $this->model = $task;
+    }
+
+    public function create($params)
+    {
+        try {
+            $params['status'] = 1;
+            
+            return $this->model->create($params);
+        } catch (Exception $exception) {
+            Log::error($exception);
+
+            return false;
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['prefix'=>'tasks','as'=>'tasks.'], function() {
+    Route::get('/', [TaskController::class, 'index']);
+    Route::post('/', [TaskController::class, 'store']);
+    Route::get('/{task}', [TaskController::class, 'show']);
 });
